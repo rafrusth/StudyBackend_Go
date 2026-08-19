dddce4a19cf2d5ebb5addab4e6f2475fb3b5c7cf
rafrusth <rafifsetya41@gmail.com>
2026-08-19 19:39:07 +0700

    add: short_variable_declaration

---
+++ ShortVariableDeclaration/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Deklarasi
+	messageStart := "Happy birthday! You are now"
+	age := 21
+	messageEnd := "years old!"
+
+	// Algoritma
+	fmt.Println(messageStart, age, messageEnd)
+}
